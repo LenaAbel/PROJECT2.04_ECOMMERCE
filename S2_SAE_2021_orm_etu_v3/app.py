4ccca550a65e9f05e8a6942ef53ac0a23b987962
SAEADPT <98150930+SAEADPT@users.noreply.github.com>
2022-01-28 08:55:22 +0100

    Update app.py

--- S2_SAE_2021_orm_etu_v3/app.py
+++ S2_SAE_2021_orm_etu_v3/app.py
@@ -4,30 +4,19 @@
 from flask import Flask, request, render_template, redirect, url_for, abort, flash, session, g
 from flask import Blueprint
 
+
 from controllers.auth_security import *
 
-from controllers.client_chaussure import *
+from controllers.client_article import *
 from controllers.client_panier import *
 from controllers.client_commande import *
 from controllers.client_commentaire import *
 
-from controllers.admin_chaussure import *
+from controllers.admin_article import *
 from controllers.admin_commande import *
 from controllers.admin_panier import *
-from controllers.admin_type_chaussure import *
-from controllers.admin_dataviz_chaussure import *
-
-import pymysql.cursors
-
-mydb = pymysql.connect(    #pymysql.connect remplace mysql.connector
-  host="serveurmysql",   #serveurmysql
-  user="label2",
-  password="0310",
-  database="BDD_label2",
-  charset='utf8mb4',                      # 2 attributs à ajouter
-  cursorclass=pymysql.cursors.DictCursor  # 2 attributs à ajouter
-)
-mycursor = mydb.cursor()
+from controllers.admin_type_article import *
+from controllers.admin_dataviz_article import *
 
 app = Flask(__name__)
 app.secret_key = 'une cle(token) : grain de sel(any random string)'
@@ -39,100 +28,45 @@ def close_connection(exception):
     if db is not None:
         db.close()
 
+
 @app.route('/')
 def show_accueil():
     return render_template('auth/layout.html')
 
-###################ADMIN CHAUSSURE###########################
+##################
+# Authentification
+##################
 
-@app.route('/admin')
-def show_accueil_admin():
-    return render_template('admin/layout_admin.html')
+# Middleware de sécurité
 
-@app.route('/admin/chaussure/add_chaussure', methods=['GET'])
-def add_chaussure():
-    k
-@app.route('/admin/chaussure/add_chaussure', methods=['POST'])
-def valid_add_chaussure():
-    k
-@app.route('/admin/chaussure/edit_chaussure', methods=['GET'])
-def edit_chaussure():
-    k
+@app.before_request
+def before_request():
+     if request.path.startswith('/admin') or request.path.startswith('/client'):
+        if 'role' not in session:
+            return redirect('/login')
+            #return redirect(url_for('auth_login'))
+        else:
+            if (request.path.startswith('/client') and session['role'] != 'ROLE_client') or (request.path.startswith('/admin') and session['role'] != 'ROLE_admin'):
+                print('pb de route : ', session['role'], request.path.title(), ' => deconnexion')
+                session.pop('username', None)
+                session.pop('role', None)
+                return redirect('/login')
+                #return redirect(url_for('auth_login'))
 
-@app.route('/admin/chaussure/edit_chaussure', methods=['POST'])
-def valid_edit_chaussure():
-    k
-    
-@app.route('/admin/chaussure/show_chaussure')
-def show_chaussure():
-    k
-    
-########ADMIN DATA VIZ#######
-    
-@app.route('/dataviz/etat_chaussure_vente')
-def etat_chaussure_vente():
-    return render_template('/admin/dataviz/etat_chaussure_vente.html')
-@app.route('/dataviz/etat_type_chaussure_stock')
-def etat_type_chaussure_stock():
-    k
-    
-#####ADMIN TYPE CHAUSSURE######    
 
-@app.route('/type_chaussure/add_type_chaussure', methods=['GET'])
-def add_type_chaussure():
-    k
-@app.route('/type_chaussure/add_type_chaussure', methods=['POST'])
-def valid_add_type_chaussure():
-    k
-@app.route('/type_chaussure/delete_type_chaussure')
-def delete_type_chaussure():
-    k
-@app.route('/type_chaussure/edit_type_chaussure', methods=['GET'])
-def edit_type_chaussure():
-    k
-@app.route('/type_chaussure/edit_type_chaussure', methods=['POST'])
-def valid_edit_type_chaussure():
-    k
-@app.route('/type_chaussure/show_type_chaussure')
-def show_type_chaussure():
-    k
-#######ADMIN COMMANDES#######
-@app.route('/commandes/show')
-def show_commandes():    
-    k
-    
-#############AUTH##############################
+app.register_blueprint(auth_security)
 
-@app.route('/auth/layout')
-def layout():
-    k
-@app.route('/auth/login')
-def login():
-    k
-@app.route('/auth/signup')
-def signup():
-    k
-    
-##################CLIENT#########################
-@app.route('/client')
-def show_accueil():
-    return render_template('auth/layout.html')
+app.register_blueprint(client_article)
+app.register_blueprint(client_commande)
+app.register_blueprint(client_commentaire)
+app.register_blueprint(client_panier)
 
-#########BOUTIQUE###########
-@app.route('/client/boutique/_filtre')
-def _filtre():
-    k
-@app.route('/client/boutique/chaussure_details')
-def chaussure_details():
-    k
-@app.route('/client/boutique/panier_chaussure')
-def panier_chaussure():
-    k
-    
-#########COMMANDES###########
-@app.route('/client/commandes/show')
-def show_commandes_client():
-    k
-    
+app.register_blueprint(admin_article)
+app.register_blueprint(admin_commande)
+app.register_blueprint(admin_panier)
+app.register_blueprint(admin_type_article)
+app.register_blueprint(admin_dataviz_article)
+
+
 if __name__ == '__main__':
     app.run()
