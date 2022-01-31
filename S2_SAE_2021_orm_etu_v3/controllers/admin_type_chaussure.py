3211522b24ba6b1929c42d13454e17d2b881083d
SAEADPT <98150930+SAEADPT@users.noreply.github.com>
2022-01-31 09:15:53 +0100

    Update admin_type_chaussure.py

--- S2_SAE_2021_orm_etu_v3/controllers/admin_type_chaussure.py
+++ S2_SAE_2021_orm_etu_v3/controllers/admin_type_chaussure.py
@@ -5,45 +5,45 @@ from flask import Flask, request, render_template, redirect, url_for, abort, fla
 
 from connexion_db import get_db
 
-admin_type_article = Blueprint('admin_type_article', __name__,
+admin_type_chaussure = Blueprint('admin_type_chaussure', __name__,
                         template_folder='templates')
 
-@admin_type_article.route('/admin/type-article/show')
-def show_type_article():
+@admin_type_chaussure.route('/admin/type-chaussure/show')
+def show_type_chaussure():
     mycursor = get_db().cursor()
-    types_articles = []
-    return render_template('admin/type_article/show_type_article.html', types_articles=types_articles)
+    types_chaussures = []
+    return render_template('admin/type_chaussure/show_type_chaussure.html', types_chaussures=types_chaussures)
 
-@admin_type_article.route('/admin/type-article/add', methods=['GET'])
-def add_type_article():
-    return render_template('admin/type_article/add_type_article.html')
+@admin_type_chaussure.route('/admin/type-chaussure/add', methods=['GET'])
+def add_type_chaussure():
+    return render_template('admin/type_chaussure/add_type_chaussure.html')
 
-@admin_type_article.route('/admin/type-article/add', methods=['POST'])
-def valid_add_type_article():
+@admin_type_chaussure.route('/admin/type-chaussure/add', methods=['POST'])
+def valid_add_type_chaussure():
     libelle = request.form.get('libelle', '')
     tuple_insert = (libelle,)
     message = u'type ajouté , libellé :'+libelle
     flash(message)
-    return redirect('/admin/type-article/show') #url_for('show_type_article')
+    return redirect('/admin/type-chaussure/show') #url_for('show_type_chaussure')
 
-@admin_type_article.route('/admin/type-article/delete', methods=['GET'])
-def delete_type_article():
-    id_type_article = request.args.get('id', '')
-    flash(u'suppression type article , id : ' + id_type_article)
-    return redirect('/admin/type-article/show') #url_for('show_type_article')
+@admin_type_chaussure.route('/admin/type-chaussure/delete', methods=['GET'])
+def delete_type_chaussure():
+    id_type_chaussure = request.args.get('id', '')
+    flash(u'suppression type chaussure , id : ' + id_type_chaussure)
+    return redirect('/admin/type-chaussure/show') #url_for('show_type_chaussure')
 
-@admin_type_article.route('/admin/type-article/edit/<int:id>', methods=['GET'])
-def edit_type_article(id):
+@admin_type_chaussure.route('/admin/type-chaussure/edit/<int:id>', methods=['GET'])
+def edit_type_chaussure(id):
     mycursor = get_db().cursor()
-    type_article = []
-    return render_template('admin/type_article/edit_type_article.html', type_article=type_article)
+    type_chaussure = []
+    return render_template('admin/type_chaussure/edit_type_chaussure.html', type_chaussure=type_chaussure)
 
-@admin_type_article.route('/admin/type-article/edit', methods=['POST'])
-def valid_edit_type_article():
+@admin_type_chaussure.route('/admin/type-chaussure/edit', methods=['POST'])
+def valid_edit_type_chaussure():
     libelle = request.form['libelle']
-    id_type_article = request.form.get('id', '')
-    flash(u'type article modifié, id: ' + id_type_article + " libelle : " + libelle)
-    return redirect('/admin/type-article/show') #url_for('show_type_article')
+    id_type_chaussure = request.form.get('id', '')
+    flash(u'type chaussure modifié, id: ' + id_type_chaussure + " libelle : " + libelle)
+    return redirect('/admin/type-chaussure/show') #url_for('show_type_chaussure')
 
 
 
