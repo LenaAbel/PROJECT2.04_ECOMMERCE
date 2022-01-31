b3439524f7c3675f9cf820d7e61d0d6b25829755
SAEADPT <98150930+SAEADPT@users.noreply.github.com>
2022-01-31 09:16:48 +0100

    Update client_chaussure.py

--- S2_SAE_2021_orm_etu_v3/controllers/client_chaussure.py
+++ S2_SAE_2021_orm_etu_v3/controllers/client_chaussure.py
@@ -2,26 +2,26 @@
 # -*- coding:utf-8 -*-
 from flask import Blueprint
 from flask import Flask, request, render_template, redirect, url_for, abort, flash, session, g
-
+from datetime import datetime
 from connexion_db import get_db
 
-client_chaussure = Blueprint('client_chaussure', __name__,
+client_commande = Blueprint('client_commande', __name__,
                         template_folder='templates')
 
-@client_chaussure.route('/client/index')
-@client_chaussure.route('/client/chaussure/show')      # remplace /client
-def client_chaussure_show():                                 # remplace client_index
-    mycursor = get_db().cursor()
-    chaussures = []
-    types_chaussures = []
-    chaussures_panier = []
-    prix_total = None
-    return render_template('client/boutique/panier_chaussure.html', chaussures=chaussures, chaussuresPanier=chaussures_panier, prix_total=prix_total, itemsFiltre=types_chaussures)
 
-@client_chaussure.route('/client/chaussure/details/<int:id>', methods=['GET'])
-def client_chaussure_details(id):
+@client_commande.route('/client/commande/add', methods=['POST'])
+def client_commande_add():
     mycursor = get_db().cursor()
-    chaussure=None
-    commentaires=None
-    commandes_chaussures=None
-    return render_template('client/boutique/chaussure_details.html', chaussure=chaussure, commentaires=commentaires, commandes_chaussures=commandes_chaussures)
+    flash(u'Commande ajoutée')
+    return redirect('/client/chaussure/show')
+    #return redirect(url_for('client_index'))
+
+
+
+@client_commande.route('/client/commande/show', methods=['get','post'])
+def client_commande_show():
+    mycursor = get_db().cursor()
+    commandes = None
+    chaussures_commande = None
+    return render_template('client/commandes/show.html', commandes=commandes, chaussures_commande=chaussures_commande)
+
