f62e87ee41f2be855c2699a7a146f257bcfc64fd
SAEADPT <98150930+SAEADPT@users.noreply.github.com>
2022-02-03 15:30:45 +0100

    Update app.py

--- S2_SAE_2021_orm_etu_v3/app.py
+++ S2_SAE_2021_orm_etu_v3/app.py
@@ -4,19 +4,19 @@
 from flask import Flask, request, render_template, redirect, url_for, abort, flash, session, g
 from flask import Blueprint
 
-
+from controllers import client_chaussure, admin_type_chaussure
 from controllers.auth_security import *
 
-from controllers.client_article import *
+from controllers.client_chaussure import *
 from controllers.client_panier import *
 from controllers.client_commande import *
 from controllers.client_commentaire import *
 
-from controllers.admin_article import *
+from controllers.admin_chaussure import *
 from controllers.admin_commande import *
 from controllers.admin_panier import *
-from controllers.admin_type_article import *
-from controllers.admin_dataviz_article import *
+from controllers.admin_type_chaussure import *
+from controllers.admin_dataviz_chaussure import *
 
 app = Flask(__name__)
 app.secret_key = 'une cle(token) : grain de sel(any random string)'
@@ -56,16 +56,16 @@ def before_request():
 
 app.register_blueprint(auth_security)
 
-app.register_blueprint(client_article)
+app.register_blueprint(client_chaussure)
 app.register_blueprint(client_commande)
 app.register_blueprint(client_commentaire)
 app.register_blueprint(client_panier)
 
-app.register_blueprint(admin_article)
+app.register_blueprint(admin_chaussure)
 app.register_blueprint(admin_commande)
 app.register_blueprint(admin_panier)
-app.register_blueprint(admin_type_article)
-app.register_blueprint(admin_dataviz_article)
+app.register_blueprint(admin_type_chaussure)
+app.register_blueprint(admin_dataviz_chaussure)
 
 
 if __name__ == '__main__':
