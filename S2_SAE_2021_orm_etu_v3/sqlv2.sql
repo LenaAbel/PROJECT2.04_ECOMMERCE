6086e8f3b00efc607f3c9c5f649304cb13d76f6c
SAEADPT <98150930+SAEADPT@users.noreply.github.com>
2022-02-03 09:24:03 +0100

    Update sqlv2.sql

--- S2_SAE_2021_orm_etu_v3/sqlv2.sql
+++ S2_SAE_2021_orm_etu_v3/sqlv2.sql
@@ -62,6 +62,7 @@ CREATE TABLE IF NOT EXISTS CHAUSSURE(
    nom_chaussure VARCHAR(50),
    marque_chaussure VARCHAR(50),
    fournisseur_chaussure VARCHAR(50),
+   image_chaussure VARCHAR(50),
    id_type_chaussure INT,
    PRIMARY KEY(id_chaussure),
    FOREIGN KEY(id_type_chaussure) REFERENCES TYPE_CHAUSSURE(id_type_chaussure)
@@ -105,19 +106,25 @@ CREATE TABLE IF NOT EXISTS LIGNE_COMMANDE(
    FOREIGN KEY(id_chaussure) REFERENCES CHAUSSURE(id_chaussure)
 )CHARACTER SET 'utf8';
 
-INSERT INTO UTILISATEUR VALUES (NULL, 'Garou', 'Loup', 'ROLE_client', 1, 'Garou42', 'garoudu42@gmail.com'),
-                               (NULL, 'Jean-Charles', 'Eude', 'ROLE_client', 1, 'JC56', 'jean.charles@gmail.com'),
-                               (NULL, 'admin', 'admin', 'ROLE_admin', 1, 'admin', 'admin@admin.fr'),  #sha256$pBGlZy6UukyHBFDH$2f089c1d26f2741b68c9218a68bfe2e25dbb069c27868a027dad03bcb3d7f69a
-                               (NULL, 'client', 'client', 'ROLE_client', 1, 'client', 'client@client.fr'), #sha256$Q1HFT4TKRqnMhlTj$cf3c84ea646430c98d4877769c7c5d2cce1edd10c7eccd2c1f9d6114b74b81c4
-                               (NULL, 'client2', 'client2', 'ROLE_client',1, 'client2', 'client2@client2.fr'), #sha256$ayiON3nJITfetaS8$0e039802d6fac2222e264f5a1e2b94b347501d040d71cfa4264cad6067cf5cf3
-                               (NULL, 'Nicolas Picot', '2012', 'ROLE_admin', 1, 'Nicolas', 'nicolas.picot@gmail.com'),
-                               (NULL, 'Lucas Dubol', '1705', 'ROLE_admin', 1, 'Lucas', 'lucas.dubol@gmail.com'),
-                               (NULL, 'Lena Abel', '0310', 'ROLE_admin', 1, 'Lena', 'lena.abel@gmail.com'),
-                               (NULL, 'Samuel Toillon', '3006', 'ROLE_admin', 1, 'Samuel', 'samuel.toillon@gmail.com');
+INSERT INTO UTILISATEUR VALUES (NULL, 'admin', 'sha256$pBGlZy6UukyHBFDH$2f089c1d26f2741b68c9218a68bfe2e25dbb069c27868a027dad03bcb3d7f69a', 'ROLE_admin', 1, 'admin', 'admin@admin.fr'),
+                               (NULL, 'client', 'sha256$Q1HFT4TKRqnMhlTj$cf3c84ea646430c98d4877769c7c5d2cce1edd10c7eccd2c1f9d6114b74b81c4', 'ROLE_client', 1, 'client', 'client@client.fr'),
+                               (NULL, 'client2', 'sha256$ayiON3nJITfetaS8$0e039802d6fac2222e264f5a1e2b94b347501d040d71cfa4264cad6067cf5cf3', 'ROLE_client',1, 'client2', 'client2@client2.fr');
 
 INSERT INTO ETAT VALUES (NULL,'Expediée'),
                         (NULL,'En Cours'),
-                        (NULL,'Recu');
+                        (NULL,'Reçu');
+
+INSERT INTO COMMANDE VALUES (NULL, '2019-12-20', 2, 3),
+                            (NULL, '2021-02-03', 1, 3),
+                            (NULL, '2021-09-29', 3, 2),
+                            (NULL, '2021-12-07', 1, 1),
+                            (NULL, '2019-05-19', 1, 3),
+                            (NULL, '2022-01-25', 2, 1),
+                            (NULL, '2021-11-11', 2, 2),
+                            (NULL, '2020-12-25', 1, 3),
+                            (NULL, '2020-07-12', 3, 2),
+                            (NULL, '2020-01-05', 3, 3);
+
 
 INSERT INTO TYPE_CHAUSSURE VALUES (NULL, 'Baskets'),
                                   (NULL, 'Claquettes'),
@@ -139,16 +146,6 @@ INSERT INTO COULEUR VALUES (NULL, 'Beige'),
                            (NULL, 'Rose'),
                            (NULL, 'Orange');
 
-INSERT INTO COMMANDE VALUES (NULL, '2019-12-20', 2, 3),
-                            (NULL, '2021-02-03', 4, 3),
-                            (NULL, '2021-09-29', 9, 2),
-                            (NULL, '2021-12-07', 7, 1),
-                            (NULL, '2019-05-19', 8, 3),
-                            (NULL, '2022-01-25', 6, 1),
-                            (NULL, '2021-11-11', 5, 2),
-                            (NULL, '2020-12-25', 7, 3),
-                            (NULL, '2020-07-12', 5, 2),
-                            (NULL, '2020-01-05', 1, 3);
 
 INSERT INTO POINTURE VALUES (NULL, 36.0),
                             (NULL, 36.5),
@@ -176,18 +173,18 @@ INSERT INTO POINTURE VALUES (NULL, 36.0),
                             (NULL, 47.5),
                             (NULL, 48.0);
 
-INSERT INTO CHAUSSURE VALUES (NULL, 'Oui', 'NIKE', 'Point C', 1),
-                             (NULL, 'Non', 'Balenciago', 'Botru-Chausse', 2),
-                             (NULL, 'Corail', 'Cannonne', 'Drof', 4),
-                             (NULL, 'Alpha', 'Letin', 'Nozama', 6),
-                             (NULL, 'Zilla', 'Nokin', 'Eaki', 3),
-                             (NULL, 'Ethernal', 'Ssegue', 'Odentinne', 5),
-                             (NULL, 'Octo', 'NIKE', 'Point C', 1),
-                             (NULL, 'Virtuos', 'Balenciago', 'Botru-Chausse', 2),
-                             (NULL, 'Elite', 'Cannonne', 'Drof', 4),
-                             (NULL, 'Digi', 'Letin', 'Nozama', 6),
-                             (NULL, 'Star', 'Nokin', 'Eaki', 3),
-                             (NULL, 'Joker', 'Ssegue', 'Odentinne', 5);
+INSERT INTO CHAUSSURE VALUES (NULL, 'Oui', 'NIKE', 'Point C', NULL, 1),
+                             (NULL, 'Non', 'Balenciago', 'Botru-Chausse',NULL, 1),
+                             (NULL, 'Corail', 'Cannonne', 'Drof', NULL, 4),
+                             (NULL, 'Alpha', 'Letin', 'Nozama', NULL, 6),
+                             (NULL, 'Zilla', 'Nokin', 'Eaki', NULL, 3),
+                             (NULL, 'Ethernal', 'Ssegue', 'Odentinne', NULL, 5),
+                             (NULL, 'Octo', 'NIKE', 'Point C', NULL, 2),
+                             (NULL, 'Virtuos', 'Balenciago', 'Botru-Chausse', NULL, 2),
+                             (NULL, 'Elite', 'Cannonne', 'Drof', NULL, 4),
+                             (NULL, 'Digi', 'Letin', 'Nozama', NULL, 6),
+                             (NULL, 'Star', 'Nokin', 'Eaki', NULL, 3),
+                             (NULL, 'Joker', 'Ssegue', 'Odentinne', NULL, 5);
 
 INSERT INTO LIGNE_COMMANDE VALUES (1, 1, 49.99, 1),
                                   (2, 2, 79.99, 2),
@@ -218,28 +215,26 @@ INSERT INTO MESURE VALUES (1, 18),
 INSERT INTO EST_DE_COULEUR VALUES (1, 11),
                                   (2, 2),
                                   (3, 3),
-                                   (4, 11),
-                                   (5, 10),
-                                   (6, 7),
-                                   (7, 8),
-                                   (8, 4),
-                                   (9, 10),
-                                   (10, 7),
-                                   (11, 2),
-                                   (12, 10)
-);
+                                  (4, 11),
+                                  (5, 10),
+                                  (6, 7),
+                                  (7, 8),
+                                  (8, 4),
+                                  (9, 10),
+                                  (10, 7),
+                                  (11, 2),
+                                  (12, 10);
 
 INSERT INTO PANIER VALUES (NULL, '2019-12-19', 49.99, 1, 1, 1),
-                          (NULL, '202 0-01-05', 79.99, 2, 2, 2),
-                          (NULL, '2020-01-05', 69.99, 1, 3, 4),
-                          (NULL, '2020-01-05', 35.99, 2, 10, 6),
-                          (NULL, '2020-01-05', 99.99, 1, 11, 7),
-                          (NULL, '2020-01-05', 49.99, 1, 4, 3),
-                          (NULL, '2020-01-05', 59.99, 1, 9, 9),
-                          (NULL, '2020-01-05', 39.99, 2, 5, 5),
-                          (NULL, '2020-01-05', 29.99, 2, 8, 7),
-                          (NULL, '2020-01-05', 24.99, 1, 6, 8);
-
+                          (NULL, '2020-01-05', 79.99, 2, 2, 1),
+                          (NULL, '2020-01-05', 69.99, 1, 3, 2),
+                          (NULL, '2020-01-05', 35.99, 2, 10, 2),
+                          (NULL, '2020-01-05', 99.99, 1, 11, 1),
+                          (NULL,' 2020-01-05', 49.99, 1, 4, 2),
+                          (NULL, '2020-01-05', 59.99, 1, 9, 3),
+                          (NULL, '2020-01-05', 39.99, 2, 5, 1),
+                          (NULL, '2020-01-05', 29.99, 2, 8, 2),
+                          (NULL, '2020-01-05', 24.99, 1, 6, 1);
 SELECT * FROM UTILISATEUR;
 SELECT * FROM ETAT;
 SELECT * FROM TYPE_CHAUSSURE;
@@ -251,3 +246,4 @@ SELECT * FROM LIGNE_COMMANDE;
 SELECT * FROM MESURE;
 SELECT * FROM EST_DE_COULEUR;
 SELECT * FROM PANIER;
+
