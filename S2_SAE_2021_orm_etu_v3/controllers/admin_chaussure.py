d8b39d9c5dbe3ea578bb1a8e842d411f2a89316c
SAEADPT <SAE204ADPT@gmail.com>
2022-02-06 15:11:35 +0100

    Ajout de add_chaussure

--- S2_SAE_2021_orm_etu_v3/controllers/admin_chaussure.py
+++ S2_SAE_2021_orm_etu_v3/controllers/admin_chaussure.py
@@ -1,9 +1,10 @@
 #! /usr/bin/python
 # -*- coding:utf-8 -*-
 from flask import Blueprint
-from flask import Flask, request, render_template, redirect, url_for, abort, flash, session, g
+from flask import request, render_template, redirect, url_for, flash
 
 from connexion_db import get_db
+
 
 admin_chaussure = Blueprint('admin_chaussure', __name__,
                         template_folder='templates')
@@ -14,6 +15,7 @@ def show_chaussure():
     sql = "SELECT * FROM CHAUSSURE"
     mycursor.execute(sql)
     chaussures = mycursor.fetchall()
+    print("In show_chaussure(): ")
     print(chaussures)
     return render_template('admin/chaussure/show_chaussure.html', chaussures=chaussures)
 
@@ -23,22 +25,27 @@ def add_chaussure():
     sql = "SELECT * FROM TYPE_CHAUSSURE"
     mycursor.execute(sql)
     types_chaussures = mycursor.fetchall()
+    print("In add_chaussure(): ")
     print(types_chaussures)
-    return render_template('admin/chaussure/add_chaussure.html', type_chaussure=types_chaussures)
+    return render_template('admin/chaussure/add_chaussure.html', types_chaussures=types_chaussures)
 
 @admin_chaussure.route('/admin/chaussure/add', methods=['POST'])
 def valid_add_chaussure():
-    nom = request.form.get('nom', '')
-    type_chaussure_id = request.form.get('type_chaussure_id', '')
-   # type_chaussure_id = int(type_chaussure_id)
-    prix = request.form.get('prix', '')
-    stock = request.form.get('stock', '')
-    description = request.form.get('description', '')
-    image = request.form.get('image', '')
-
-
-    print(u'chaussure ajouté , nom: ', nom, ' - type_chaussure:', type_chaussure_id, ' - prix:', prix, ' - stock:', stock, ' - description:', description, ' - image:', image)
-    message = u'chaussure ajouté , nom:'+nom + '- type_chaussure:' + type_chaussure_id + ' - prix:' + prix + ' - stock:'+  stock + ' - description:' + description + ' - image:' + image
+    nom         = request.form.get('nom_chaussure', '')
+    id_type     = request.form.get('id_type_chaussure', '')
+    prix        = request.form.get('prix_chaussure', '')
+    stock       = request.form.get('stock_chaussure', '')
+    marque      = request.form.get('marque_chaussure', '')
+    fournisseur = request.form.get('fournisseur_chaussure', '')
+    #description= request.form.get('description', '')
+    image       = request.form.get('image_chaussure', '')
+    tuple_insert= (nom,id_type,prix,stock,marque,fournisseur,image)
+    sql = "INSERT INTO CHAUSSURE(nom_chaussure, id_type_chaussure, prix_chaussure,stock_chaussure,marque_chaussure, fournisseur_chaussure, image_chaussure) VALUES (%s, %s, %s, %s, %s, %s, %s);"
+    mycursor = get_db().cursor()
+    res = mycursor.execute(sql, tuple_insert)
+    res = get_db().commit()
+    #print(u'Chaussure ajoutée, Nom: ', nom, ' - Type chaussure:', id_type, ' - Prix:', prix, ' - Stock:', stock,' - Marque:', marque, ' - Fournisseur:', fournisseur, ' - Image:', image)
+    message = u'Chaussure ajoutée, Nom:'+nom + '- Type chaussure:' + id_type + ' - Prix:' + prix + ' - Stock:'+  stock + ' - Marque:'+ marque+' - Fournisseur:'+fournisseur+ ' - Image:' + image
     flash(message)
     return redirect(url_for('admin_chaussure.show_chaussure'))
 
