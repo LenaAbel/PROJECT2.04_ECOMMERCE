33858e30321581c2ffac988c43054ae473650e19
SAEADPT <SAE204ADPT@gmail.com>
2022-02-11 10:01:44 +0100

    lets go

--- S2_SAE_2021_orm_etu_v3/controllers/admin_chaussure.py
+++ S2_SAE_2021_orm_etu_v3/controllers/admin_chaussure.py
@@ -15,9 +15,21 @@ def show_chaussure():
     sql = "SELECT * FROM CHAUSSURE"
     mycursor.execute(sql)
     chaussures = mycursor.fetchall()
+    sql = "SELECT * FROM TYPE_CHAUSSURE"
+    mycursor.execute(sql)
+    types_chaussures = mycursor.fetchall()
+    sql = "SELECT * FROM FOURNISSEUR"
+    mycursor.execute(sql)
+    fournisseurs = mycursor.fetchall()
+    sql = "SELECT * FROM MARQUE"
+    mycursor.execute(sql)
+    marques = mycursor.fetchall()
+    sql = "SELECT * FROM MATIERE"
+    mycursor.execute(sql)
+    matieres = mycursor.fetchall()
     print("In show_chaussure(): ")
     print(chaussures)
-    return render_template('admin/chaussure/show_chaussure.html', chaussures=chaussures)
+    return render_template('admin/chaussure/show_chaussure.html', chaussures=chaussures, types_chaussures=types_chaussures, fournisseurs=fournisseurs, marques=marques, matieres=matieres)
 
 @admin_chaussure.route('/admin/chaussure/add', methods=['GET'])
 def add_chaussure():
@@ -25,9 +37,21 @@ def add_chaussure():
     sql = "SELECT * FROM TYPE_CHAUSSURE"
     mycursor.execute(sql)
     types_chaussures = mycursor.fetchall()
+    sql = "SELECT * FROM FOURNISSEUR"
+    mycursor.execute(sql)
+    fournisseurs = mycursor.fetchall()
+    sql = "SELECT * FROM MARQUE"
+    mycursor.execute(sql)
+    marques = mycursor.fetchall()
+    sql = "SELECT * FROM MATIERE"
+    mycursor.execute(sql)
+    matieres = mycursor.fetchall()
+
     print("In add_chaussure(): ")
     print(types_chaussures)
-    return render_template('admin/chaussure/add_chaussure.html', types_chaussures=types_chaussures)
+    print(fournisseurs)
+    print(marques)
+    return render_template('admin/chaussure/add_chaussure.html', types_chaussures=types_chaussures, fournisseurs=fournisseurs, marques=marques, matieres=matieres)
 
 @admin_chaussure.route('/admin/chaussure/add', methods=['POST'])
 def valid_add_chaussure():
@@ -37,10 +61,11 @@ def valid_add_chaussure():
     stock       = request.form.get('stock_chaussure', '')
     marque      = request.form.get('marque_chaussure', '')
     fournisseur = request.form.get('fournisseur_chaussure', '')
+    matiere = request.form.get('matiere_chaussure', '')
     #description= request.form.get('description', '')
     image       = request.form.get('image_chaussure', '')
-    tuple_insert= (nom,id_type,prix,stock,marque,fournisseur,image)
-    sql = "INSERT INTO CHAUSSURE(nom_chaussure, id_type_chaussure, prix_chaussure,stock_chaussure,marque_chaussure, fournisseur_chaussure, image_chaussure) VALUES (%s, %s, %s, %s, %s, %s, %s);"
+    tuple_insert= (nom,id_type,prix,stock,marque,fournisseur,matiere,image)
+    sql = "INSERT INTO CHAUSSURE(nom_chaussure, id_type_chaussure, prix_chaussure,stock_chaussure,id_marque,id_fournisseur, id_matiere , image_chaussure) VALUES (%s, %s, %s, %s, %s, %s, %s, %s);"
     mycursor = get_db().cursor()
     res = mycursor.execute(sql, tuple_insert)
     res = get_db().commit()
@@ -51,9 +76,45 @@ def valid_add_chaussure():
 
 @admin_chaussure.route('/admin/chaussure/delete', methods=['GET'])
 def delete_chaussure():
-    #id = request.args.get('id_chaussure', '')
-    id = request.form.get('id_chaussure', '')
+    id = request.args.get('id', '')
+    #id = request.form.get('id', '')
     tuple_delete = (id)
+    sql = "DELETE FROM LIGNE_COMMANDE WHERE id_chaussure = %s;"
+    mycursor = get_db().cursor()
+    # mycursor.execute("SET FOREIGN_KEY_CHECKS=0")
+    mycursor.execute(sql, tuple_delete)
+    get_db().commit()
+
+    sql = "DELETE FROM NOTE WHERE id_chaussure = %s;"
+    mycursor = get_db().cursor()
+    # mycursor.execute("SET FOREIGN_KEY_CHECKS=0")
+    mycursor.execute(sql, tuple_delete)
+    get_db().commit()
+
+    sql = "DELETE FROM COMMENTAIRE WHERE id_chaussure = %s;"
+    mycursor = get_db().cursor()
+    # mycursor.execute("SET FOREIGN_KEY_CHECKS=0")
+    mycursor.execute(sql, tuple_delete)
+    get_db().commit()
+
+    sql = "DELETE FROM EST_DE_COULEUR WHERE id_chaussure = %s;"
+    mycursor = get_db().cursor()
+    # mycursor.execute("SET FOREIGN_KEY_CHECKS=0")
+    mycursor.execute(sql, tuple_delete)
+    get_db().commit()
+
+    sql = "DELETE FROM PANIER WHERE id_chaussure = %s;"
+    mycursor = get_db().cursor()
+    # mycursor.execute("SET FOREIGN_KEY_CHECKS=0")
+    mycursor.execute(sql, tuple_delete)
+    get_db().commit()
+
+    sql = "DELETE FROM MESURE WHERE id_chaussure = %s;"
+    mycursor = get_db().cursor()
+    # mycursor.execute("SET FOREIGN_KEY_CHECKS=0")
+    mycursor.execute(sql, tuple_delete)
+    get_db().commit()
+
     sql = "DELETE FROM CHAUSSURE WHERE id_chaussure = %s;"
     mycursor = get_db().cursor()
     #mycursor.execute("SET FOREIGN_KEY_CHECKS=0")
@@ -65,9 +126,6 @@ def delete_chaussure():
 
 @admin_chaussure.route('/admin/chaussure/edit/<int:id>', methods=['GET'])
 def edit_chaussure(id):
-    """ Edite une chaussure
-        @param id L'ID de la chaussure à éditer.
-    """
     sql="SELECT * FROM CHAUSSURE WHERE id_chaussure=%s"
     mycursor = get_db().cursor()
     mycursor.execute(sql, id)
@@ -75,7 +133,16 @@ def edit_chaussure(id):
     sql="SELECT * FROM TYPE_CHAUSSURE"
     mycursor.execute(sql)
     types_chaussures = mycursor.fetchall()
-    return render_template('admin/chaussure/edit_chaussure.html', chaussure=chaussure, types_chaussures=types_chaussures)
+    sql = "SELECT * FROM FOURNISSEUR"
+    mycursor.execute(sql)
+    fournisseurs = mycursor.fetchall()
+    sql = "SELECT * FROM MARQUE"
+    mycursor.execute(sql)
+    marques = mycursor.fetchall()
+    sql = "SELECT * FROM MATIERE"
+    mycursor.execute(sql)
+    matieres = mycursor.fetchall()
+    return render_template('admin/chaussure/edit_chaussure.html', chaussure=chaussure, types_chaussures=types_chaussures, fournisseurs=fournisseurs, marques=marques, matieres=matieres)
 
 @admin_chaussure.route('/admin/chaussure/edit', methods=['POST'])
 def valid_edit_chaussure():
@@ -87,11 +154,11 @@ def valid_edit_chaussure():
     stock = request.form.get('stock_chaussure', '')
     marque = request.form.get('marque_chaussure', '')
     fournisseur = request.form.get('fournisseur_chaussure', '')
+    matiere = request.form.get('fournisseur_chaussure', '')
     # description= request.form.get('description', '')
     image = request.form.get('image_chaussure', '')
-    tuple_update = (nom, id_type, prix, stock, marque, fournisseur, image, id)
-    sql = "UPDATE CHAUSSURE SET nom_chaussure=%s, id_type_chaussure=%s, prix_chaussure=%s, stock_chaussure=%s, marque_chaussure=%s, fournisseur_chaussure=%s,  \
-              image_chaussure=%s  WHERE id_chaussure=%s; "
+    tuple_update = (nom, id_type, prix, stock, marque, fournisseur, matiere, image, id)
+    sql = "UPDATE CHAUSSURE SET nom_chaussure=%s, id_type_chaussure=%s, prix_chaussure=%s, stock_chaussure=%s, id_marque=%s, id_fournisseur=%s, id_matiere=%s, image_chaussure=%s  WHERE id_chaussure=%s; "
     mycursor = get_db().cursor()
     res = mycursor.execute(sql, tuple_update)
     res = get_db().commit()
--- S2_SAE_2021_orm_etu_v3/sqlv2.sql
+++ S2_SAE_2021_orm_etu_v3/sqlv2.sql
@@ -1,6 +1,8 @@
 #mysql --user=stoillon --password=3006 --host=serveurmysql --database=BDD_stoillon
 
 DROP TABLE IF EXISTS PANIER;
+DROP TABLE IF EXISTS NOTE;
+DROP TABLE IF EXISTS COMMENTAIRE;
 DROP TABLE IF EXISTS LIGNE_COMMANDE;
 DROP TABLE IF EXISTS COMMANDE;
 DROP TABLE IF EXISTS MESURE;
@@ -9,8 +11,35 @@ DROP TABLE IF EXISTS CHAUSSURE;
 DROP TABLE IF EXISTS UTILISATEUR;
 DROP TABLE IF EXISTS ETAT;
 DROP TABLE IF EXISTS TYPE_CHAUSSURE;
+DROP TABLE IF EXISTS MATIERE;
 DROP TABLE IF EXISTS COULEUR;
+DROP TABLE IF EXISTS MARQUE;
+DROP TABLE IF EXISTS FOURNISSEUR;
 DROP TABLE IF EXISTS POINTURE;
+
+CREATE TABLE IF NOT EXISTS TYPE_CHAUSSURE(
+   id_type_chaussure INT NOT NULL AUTO_INCREMENT,
+   nom_type_chaussure VARCHAR(50),
+   PRIMARY KEY(id_type_chaussure)
+)CHARACTER SET 'utf8';
+
+CREATE TABLE IF NOT EXISTS POINTURE(
+   id_pointure INT NOT NULL AUTO_INCREMENT,
+   taille INT,
+   PRIMARY KEY(id_pointure)
+)CHARACTER SET 'utf8';
+
+CREATE TABLE IF NOT EXISTS COULEUR(
+   id_couleur INT NOT NULL AUTO_INCREMENT,
+   libelle_couleur VARCHAR(50),
+   PRIMARY KEY(id_couleur)
+)CHARACTER SET 'utf8';
+
+CREATE TABLE IF NOT EXISTS MATIERE(
+   id_matiere INT NOT NULL AUTO_INCREMENT,
+   libelle_matiere VARCHAR(50),
+   PRIMARY KEY(id_matiere)
+)CHARACTER SET 'utf8';
 
 CREATE TABLE IF NOT EXISTS UTILISATEUR(
    id_utilisateur INT NOT NULL AUTO_INCREMENT,
@@ -29,85 +58,106 @@ CREATE TABLE IF NOT EXISTS ETAT(
    PRIMARY KEY(id_etat)
 )CHARACTER SET 'utf8';
 
+CREATE TABLE IF NOT EXISTS FOURNISSEUR(
+   id_fournisseur INT NOT NULL AUTO_INCREMENT,
+   libelle_fournisseur VARCHAR(50),
+   PRIMARY KEY(id_fournisseur)
+)CHARACTER SET 'utf8';
+
+
+CREATE TABLE IF NOT EXISTS MARQUE(
+   id_marque INT NOT NULL AUTO_INCREMENT,
+   libelle_marque VARCHAR(50),
+   PRIMARY KEY(id_marque)
+)CHARACTER SET 'utf8';
+
+
+CREATE TABLE IF NOT EXISTS CHAUSSURE(
+   id_chaussure INT NOT NULL AUTO_INCREMENT,
+   nom_chaussure VARCHAR(50),
+   image_chaussure VARCHAR(50),
+   prix_chaussure VARCHAR(50),
+   stock_chaussure VARCHAR(50),
+   id_marque INT NOT NULL,
+   id_fournisseur INT NOT NULL,
+   id_matiere INT NOT NULL,
+   id_type_chaussure INT NOT NULL,
+   marque_chaussure VARCHAR(50),
+   fournisseur_chaussure VARCHAR(50),
+   PRIMARY KEY(id_chaussure),
+   CONSTRAINT chaussure_ibfk_1 FOREIGN KEY(id_marque) REFERENCES MARQUE(id_marque),
+   CONSTRAINT chaussure_ibfk_2 FOREIGN KEY(id_fournisseur) REFERENCES FOURNISSEUR(id_fournisseur),
+   CONSTRAINT chaussure_ibfk_3 FOREIGN KEY(id_matiere) REFERENCES MATIERE(id_matiere),
+   CONSTRAINT chaussure_ibfk_4 FOREIGN KEY(id_type_chaussure) REFERENCES TYPE_CHAUSSURE(id_type_chaussure)
+)CHARACTER SET 'utf8';
+
+
+CREATE TABLE IF NOT EXISTS COMMENTAIRE(
+   id_commentaire INT NOT NULL AUTO_INCREMENT,
+   libelle_commentaire VARCHAR(50),
+   id_chaussure INT NOT NULL,
+   id_utilisateur INT NOT NULL,
+   PRIMARY KEY(id_commentaire),
+   CONSTRAINT commentaire_ibfk_1 FOREIGN KEY(id_chaussure) REFERENCES CHAUSSURE(id_chaussure),
+   CONSTRAINT commentaire_ibfk_2 FOREIGN KEY(id_utilisateur) REFERENCES UTILISATEUR(id_utilisateur)
+)CHARACTER SET 'utf8';
+
+CREATE TABLE IF NOT EXISTS PANIER(
+   id_panier INT NOT NULL AUTO_INCREMENT,
+   date_ajout DATE,
+   prix_unit INT,
+   quantite INT,
+   id_chaussure INT NOT NULL,
+   id_utilisateur INT NOT NULL,
+   PRIMARY KEY(id_panier),
+   CONSTRAINT panier_ibfk_1 FOREIGN KEY(id_chaussure) REFERENCES CHAUSSURE(id_chaussure),
+   CONSTRAINT panier_ibfk_2 FOREIGN KEY(id_utilisateur) REFERENCES UTILISATEUR(id_utilisateur)
+)CHARACTER SET 'utf8';
+
 CREATE TABLE IF NOT EXISTS COMMANDE(
    id_commande INT NOT NULL AUTO_INCREMENT,
    date_achat DATE,
    id_utilisateur INT NOT NULL,
    id_etat INT NOT NULL,
    PRIMARY KEY(id_commande),
-   FOREIGN KEY(id_utilisateur) REFERENCES UTILISATEUR(id_utilisateur),
-   FOREIGN KEY(id_etat) REFERENCES ETAT(id_etat)
+   CONSTRAINT commande_ibfk_1 FOREIGN KEY(id_utilisateur) REFERENCES UTILISATEUR(id_utilisateur),
+   CONSTRAINT commande_ibfk_2 FOREIGN KEY(id_etat) REFERENCES ETAT(id_etat)
 )CHARACTER SET 'utf8';
 
-CREATE TABLE IF NOT EXISTS TYPE_CHAUSSURE(
-   id_type_chaussure INT NOT NULL AUTO_INCREMENT,
-   nom_type_chaussure VARCHAR(50),
-   PRIMARY KEY(id_type_chaussure)
-)CHARACTER SET 'utf8';
-
-CREATE TABLE IF NOT EXISTS COULEUR(
-   id_couleur INT NOT NULL AUTO_INCREMENT,
-   nom_couleur VARCHAR(50),
-   PRIMARY KEY(id_couleur)
-);
-
-CREATE TABLE IF NOT EXISTS POINTURE(
-   id_pointure INT NOT NULL AUTO_INCREMENT,
-   taille NUMERIC(3,1),
-   PRIMARY KEY(id_pointure)
-)CHARACTER SET 'utf8';
-
-CREATE TABLE IF NOT EXISTS CHAUSSURE(
-   id_chaussure INT NOT NULL AUTO_INCREMENT,
-   nom_chaussure VARCHAR(50),
-   marque_chaussure VARCHAR(50),
-   fournisseur_chaussure VARCHAR(50),
-   image_chaussure VARCHAR(50),
-   prix_chaussure NUMERIC(6,2),
-   stock_chaussure INT,
-   id_type_chaussure INT,
-   PRIMARY KEY(id_chaussure),
-   FOREIGN KEY(id_type_chaussure) REFERENCES TYPE_CHAUSSURE(id_type_chaussure)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE
-)CHARACTER SET 'utf8';
-
-CREATE TABLE IF NOT EXISTS PANIER(
-   id_panier INT NOT NULL AUTO_INCREMENT,
-   date_ajout DATE,
-   prix_unit NUMERIC(6, 2),
-   quantite INT,
-   id_chaussure INT,
-   id_utilisateur INT,
-   PRIMARY KEY(id_panier),
-   FOREIGN KEY(id_chaussure) REFERENCES CHAUSSURE(id_chaussure),
-   FOREIGN KEY(id_utilisateur) REFERENCES UTILISATEUR(id_utilisateur)
+CREATE TABLE IF NOT EXISTS NOTE(
+   id_note INT NOT NULL AUTO_INCREMENT,
+   note NUMERIC(6,2) default 3,
+   id_utilisateur INT NOT NULL,
+   id_chaussure INT NOT NULL,
+   PRIMARY KEY(id_note),
+   CONSTRAINT note_ibfk_1 FOREIGN KEY(id_utilisateur) REFERENCES UTILISATEUR(id_utilisateur),
+   CONSTRAINT note_ibfk_2 FOREIGN KEY(id_chaussure) REFERENCES CHAUSSURE(id_chaussure)
 )CHARACTER SET 'utf8';
 
 CREATE TABLE IF NOT EXISTS MESURE(
    id_chaussure INT,
    id_pointure INT,
    PRIMARY KEY(id_chaussure, id_pointure),
-   FOREIGN KEY(id_chaussure) REFERENCES CHAUSSURE(id_chaussure),
-   FOREIGN KEY(id_pointure) REFERENCES POINTURE(id_pointure)
+   CONSTRAINT mesure_ibfk_1 FOREIGN KEY(id_chaussure) REFERENCES CHAUSSURE(id_chaussure),
+   CONSTRAINT mesure_ibfk_2 FOREIGN KEY(id_pointure) REFERENCES POINTURE(id_pointure)
 )CHARACTER SET 'utf8';
 
 CREATE TABLE IF NOT EXISTS EST_DE_COULEUR(
    id_chaussure INT,
    id_couleur INT,
    PRIMARY KEY(id_chaussure, id_couleur),
-   FOREIGN KEY(id_chaussure) REFERENCES CHAUSSURE(id_chaussure),
-   FOREIGN KEY(id_couleur) REFERENCES COULEUR(id_couleur)
+   CONSTRAINT estdecouleur_ibfk_1 FOREIGN KEY(id_chaussure) REFERENCES CHAUSSURE(id_chaussure),
+   CONSTRAINT estdecouleur_ibfk_2 FOREIGN KEY(id_couleur) REFERENCES COULEUR(id_couleur)
 )CHARACTER SET 'utf8';
 
 CREATE TABLE IF NOT EXISTS LIGNE_COMMANDE(
-   id_commande INT NOT NULL,
-   id_chaussure INT NOT NULL,
+   id_chaussure INT,
+   id_commande INT,
    prix_unit INT,
    quantite INT,
-   PRIMARY KEY(id_commande, id_chaussure),
-   FOREIGN KEY(id_commande) REFERENCES COMMANDE(id_commande),
-   FOREIGN KEY(id_chaussure) REFERENCES CHAUSSURE(id_chaussure)
+   PRIMARY KEY(id_chaussure, id_commande),
+   CONSTRAINT lignecommande_ibfk_1 FOREIGN KEY(id_chaussure) REFERENCES CHAUSSURE(id_chaussure),
+   CONSTRAINT lignecommande_ibfk_2 FOREIGN KEY(id_commande) REFERENCES COMMANDE(id_commande)
 )CHARACTER SET 'utf8';
 
 INSERT INTO UTILISATEUR VALUES (NULL, 'admin', 'sha256$pBGlZy6UukyHBFDH$2f089c1d26f2741b68c9218a68bfe2e25dbb069c27868a027dad03bcb3d7f69a', 'ROLE_admin', 1, 'admin', 'admin@admin.fr'),
@@ -179,18 +229,39 @@ INSERT INTO POINTURE VALUES (NULL, 36.0),
                             (NULL, 47.5),
                             (NULL, 48.0);
 
-INSERT INTO CHAUSSURE VALUES (NULL, 'Galaxy', 'NIKE', 'Point C', 'galaxy_nike.png',49.99, 20,1),
-                             (NULL, 'Illusion', 'Balenciago', 'Botru-Chausse','illusion_balenciago.png',79.99, 10, 1),
-                             (NULL, 'Corail', 'Cannonne', 'Drof', 'corail_cannonne.png', 69.99, 50,4),
-                             (NULL, 'Alpha', 'Letin', 'Nozama', 'alpha_letin.png', 49.99, 50, 6),
-                             (NULL, 'Zilla', 'Nokin', 'Eaki', 'zilla_nokin.png', 39.99, 3,3),
-                             (NULL, 'Ethernal', 'Ssegue', 'Odentinne', 'ethernal_ssegue.jpg', 25.99, 30, 5),
-                             (NULL, 'Octo', 'NIKE', 'Point C', 'octo_nike.png', 12.99,15,2),
-                             (NULL, 'Virtuos', 'Balenciago', 'Botru-Chausse', 'virtuos_balenciago.png',29.99,21, 2),
-                             (NULL, 'Elite', 'Cannonne', 'Drof', 'elite_cannonne.png', 59.99,7,4),
-                             (NULL, 'Digi', 'Letin', 'Nozama', 'digi_letin.png',  35.99,70,6),
-                             (NULL, 'Star', 'Nokin', 'Eaki', 'star_nokin.png', 99.99,5,3),
-                             (NULL, 'Joker', 'Ssegue', 'Odentinne', 'joker_ssegue.png',10.99,90, 5);
+
+INSERT INTO MATIERE VALUES (NULL, 'Cuir'),
+                           (NULL, 'Laine'),
+                           (NULL, 'Fibre synthetique');
+
+INSERT INTO FOURNISSEUR VALUES (NULL, 'Point C'),
+                               (NULL, 'Botru-Chausse'),
+                               (NULL, 'Drof'),
+                               (NULL, 'Nozama'),
+                               (NULL, 'Eaki'),
+                               (NULL, 'Odentinne');
+
+INSERT INTO MARQUE VALUES (NULL, 'NIKE'),
+                          (NULL, 'Balenciago'),
+                          (NULL, 'Cannonne'),
+                          (NULL, 'Letin'),
+                          (NULL, 'Nokin'),
+                          (NULL, 'Ssegue');
+
+INSERT INTO CHAUSSURE VALUES (NULL, 'Galaxy', 'galaxy_nike.png',49.99, 20, 1, 1, 3, 1,'NIKE', 'Point C'),
+                             (NULL, 'Illusion','illusion_balenciago.png',79.99, 10, 2, 2, 3, 1, 'Balenciago', 'Botru-Chausse'),
+                             (NULL, 'Corail', 'corail_cannonne.png', 69.99, 50, 3, 3, 1,4, 'Cannonne', 'Drof'),
+                             (NULL, 'Alpha', 'alpha_letin.png', 49.99, 50, 4, 4, 1, 6, 'Letin', 'Nozama'),
+                             (NULL, 'Zilla','zilla_nokin.png', 39.99, 3, 5, 5, 1,3, 'Nokin', 'Eaki'),
+                             (NULL, 'Ethernal', 'ethernal_ssegue.png', 25.99, 30, 6, 6, 3, 5, 'Ssegue', 'Odentine'),
+                             (NULL, 'Octo', 'octo_nike.png', 12.99,15, 1, 1, 3,2,'NIKE', 'Point C'),
+                             (NULL, 'Virtuos','virtuos_balenciago.png',29.99,21, 2, 2, 1, 2, 'Balenciago', 'Botru-Chausse'),
+                             (NULL, 'Elite', 'elite_cannonne.png', 59.99,7, 3, 3, 1,4, 'Cannonne','Drof'),
+                             (NULL, 'Digi', 'digi_letin.png',  35.99,70, 4, 4, 3,6, 'Letin','Nozama'),
+                             (NULL, 'Star','star_nokin.png', 99.99,5, 5, 5, 1,3, 'Nokin', 'Eaki'),
+                             (NULL, 'Joker','joker_ssegue.png',10.99,90, 6, 6, 2, 5, 'Ssegue', 'Odentine');
+
+
 
 INSERT INTO LIGNE_COMMANDE VALUES (1, 1, 49.99, 1),
                                   (2, 2, 79.99, 2),
@@ -204,6 +275,7 @@ INSERT INTO LIGNE_COMMANDE VALUES (1, 1, 49.99, 1),
                                   (10, 6, 25.99, 1),
                                   (11, 12, 10.99,1),
                                   (12, 7, 12.99,3);
+
 
 
 INSERT INTO MESURE VALUES (1, 18),
@@ -243,16 +315,20 @@ INSERT INTO PANIER VALUES (NULL, '2019-12-19', 49.99, 1, 1, 1),
                           (NULL, '2020-01-05', 39.99, 2, 5, 1),
                           (NULL, '2020-01-05', 29.99, 2, 8, 2),
                           (NULL, '2020-01-05', 24.99, 1, 6, 1);
+
+SELECT * FROM PANIER;
+SELECT * FROM NOTE;
+SELECT * FROM COMMENTAIRE;
+SELECT * FROM LIGNE_COMMANDE;
+SELECT * FROM COMMANDE;
+SELECT * FROM MESURE;
+SELECT * FROM EST_DE_COULEUR;
+SELECT * FROM CHAUSSURE;
 SELECT * FROM UTILISATEUR;
 SELECT * FROM ETAT;
 SELECT * FROM TYPE_CHAUSSURE;
+SELECT * FROM MATIERE;
 SELECT * FROM COULEUR;
-SELECT * FROM COMMANDE;
+SELECT * FROM MARQUE;
+SELECT * FROM FOURNISSEUR;
 SELECT * FROM POINTURE;
-SELECT * FROM CHAUSSURE;
-SELECT * FROM LIGNE_COMMANDE;
-SELECT * FROM MESURE;
-SELECT * FROM EST_DE_COULEUR;
-SELECT * FROM PANIER;
-
-SELECT id_type_chaussure, COUNT(id_type_chaussure) AS nbmChaussure FROM CHAUSSURE GROUP BY id_type_chaussure ORDER BY id_type_chaussure ASC;
