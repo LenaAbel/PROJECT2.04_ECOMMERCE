e7c7ec85b6aed55e6e8114d6bf13992f0315bbc0
SAEADPT <SAE204ADPT@gmail.com>
2022-02-21 17:30:12 +0100

    mini modif visu

--- S2_SAE_2021_orm_etu_v3/controllers/admin_commande.py
+++ S2_SAE_2021_orm_etu_v3/controllers/admin_commande.py
@@ -29,7 +29,6 @@ def admin_commande_show():
     commandes = mycursor.fetchall()
     return render_template('admin/commandes/show_commandes.html', commandes=commandes)
 
-
 @admin_commande.route('/admin/commandes/valider/<int:id_commande>', methods=['GET', 'POST'])
 def admin_commande_valider(id_commande):
     mycursor = get_db().cursor()
@@ -42,7 +41,6 @@ def admin_commande_valider(id_commande):
     print("Validation de la commande n°", id_commande)
     flash(u'Validation de la commande n°' + str(id_commande))
     return redirect(url_for('admin_commande.admin_commande_show'))
-
 
 @admin_commande.route('/admin/commandes/<int:id_commande>', methods=['GET', 'POST'])
 def admin_commande_details(id_commande):
